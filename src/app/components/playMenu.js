501df2d1f2f85ced727eb2d8afd5a8f4e4672204
unknown <aashirnawaz5@gmail.com>
2025-09-23 00:04:04 +0500

    redirected to a-z

--- src/app/components/playMenu.js
+++ src/app/components/playMenu.js
@@ -1,20 +1,31 @@
+"use client";
 import React from "react";
+import { useRouter } from "next/navigation";
 import { IoHomeOutline } from "react-icons/io5";
 import { CiCircleInfo } from "react-icons/ci";
 import { FaArrowDownAZ, FaArrowLeft, FaArrowRight } from "react-icons/fa6";
 
 const PlayMenu = ({ onPrev, onNext, onInfo }) => {
+  const router = useRouter();
+
+  const goToAZ = () => {
+    router.push("/home/a-z");
+  };
+
   return (
     <div className="absolute bottom-0 w-full shadow-md">
       <div className="flex justify-between items-end">
         {/* Left Group */}
         <div className="flex">
-          <div className="p-2 rounded-l text-center border border-gray-400">
+          <div className="p-2 rounded-l text-center border border-gray-400 cursor-pointer">
             <IoHomeOutline size={24} />
           </div>
-          <div className="p-2 rounded-r text-center border border-gray-400 border-l-0">
+          <button
+            onClick={goToAZ}
+            className="p-2 rounded-r text-center border border-gray-400 border-l-0 hover:bg-gray-200 cursor-pointer"
+          >
             <FaArrowDownAZ size={24} />
-          </div>
+          </button>
         </div>
 
         {/* Center Info (raised + bigger) */}
