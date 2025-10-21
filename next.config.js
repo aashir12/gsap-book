16826d68321038e321b44387fec8ea3b955ffea3
aashir12 <aashirnawaz5@gmail.com>
2025-10-22 00:45:36 +0500

    removed files from local and used r2

--- next.config.js
+++ next.config.js
@@ -9,6 +9,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "ocean.si.edu",
       },
+      {
+        protocol: "https",
+        hostname: "pub-3fc12084b01f4c4f920f3fefbc9570a3.r2.dev",
+      },
     ],
   },
 };
